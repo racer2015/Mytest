bc6b3e6b5ab1202312ba52c8cd64af2073ee8741
pdz48239 <pdz48239@ly.com>
2017-06-14 20:49:39 +0800

    dsad

--- src/MyTest.java
+++ src/MyTest.java
@@ -12,14 +12,7 @@ public class MyTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is 子子子子子子子子子子");
-		System.out.println("this is 1");
-		System.out.println("this is 2");
-		System.out.println("this is 3");
-		System.out.println("this is 4");
-		System.out.println("this is 5");
-		System.out.println("this is 6");
-		System.out.println("this is 7");
+		
 		System.out.println("this is 7");
 
 	}
