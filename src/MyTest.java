aa2f6063be7c7a7ec3a4025f4fbf2a9c371fd07a
pdz48239 <pdz48239@ly.com>
2017-06-14 20:46:52 +0800

    111111111111111111111

--- src/MyTest.java
+++ src/MyTest.java
@@ -13,6 +13,8 @@ public class MyTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("this is 66666666666666666");
+		System.out.println("this is 88888888888888888");
 		System.out.println("this is 子子子子子子子子子子");
 		System.out.println("this is 1");
 		System.out.println("this is 2");
