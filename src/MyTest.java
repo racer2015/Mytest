87446ba60f12326e613a3ba21f7f75145ace57b2
pdz48239 <pdz48239@ly.com>
2017-06-14 20:30:29 +0800

    测试1

---
+++ src/MyTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Racer
+ *
+ */
+public class MyTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is test");
+
+	}
+
+}
