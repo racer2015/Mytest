d5c78098198c6769cf151e9bf00d0fc08c92f538
pdz48239 <pdz48239@ly.com>
2017-06-14 20:32:16 +0800

    棋

--- src/MyTest.java
+++ src/MyTest.java
@@ -13,6 +13,7 @@ public class MyTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("this is test1111111111111");
 		System.out.println("this is test");
 
 	}
